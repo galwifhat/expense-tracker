703501ea06f49d980b0baf5c6de91673ebb79497
Ruth <cirurue@gmail.com>
2025-04-13 23:00:44 +0300

    form

--- src/Form.jsx
+++ src/Form.jsx
@@ -11,25 +11,23 @@ function Form() {
     >
       <fieldset>
         <legend>Add Expenses Here</legend>
+        <h3>Add Expense</h3>
+        <p>Enter your expense details below</p>
         <p>
           <label htmlFor=""></label>
-          <input type="text" id="" placeholder="" autoFocus/>
+          <input type="text" id="" placeholder="Expense title" autoFocus/>
         </p>
         <p>
           <label htmlFor=""></label>
-          <input type="text" id="" placeholder="" autoFocus/>
+          <input type="text" id="" placeholder="Enter expense description" autoFocus/>
         </p>
         <p>
           <label htmlFor=""></label>
-          <input type="text" id="" placeholder="" autoFocus/>
+          <input type="text" id="" placeholder="enter expense category" autoFocus/>
         </p>
         <p>
           <label htmlFor=""></label>
-          <select name="" id=""></select>
-          <optgroup>
-             <option value=""></option>
-             <option value=""></option>
-          </optgroup>
+          <input type="text" id="" placeholder="Enter amount" autoFocus/>
         </p>
         <button type="submit">Submit</button>
       </fieldset>
